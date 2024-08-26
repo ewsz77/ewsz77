a747ea5f042d54fd31462132462903b9a55d7fcf
503387955 <ER6936@outlook.com>
2024-08-26 15:35:00 +0000

    feat: initiated the project

--- scripts/proxy/utils/deployed-address.ts
+++ scripts/proxy/utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xaCF84CED4eedD053C0417082F09bf2bB2d45899E'
-export const ProxyAdmin = '0xC72182487A5D82aD21F26B02900b9B7265442355'
-export const SWTRImplementation = '0xE75eD02BA12089212471dB9CB22AdFa577ea2Af5'
+export const SWTRProxy = '0xed8555E97AdB52a3B04E6B2912a65427bEf33617'
+export const ProxyAdmin = '0x343F6bf79A51533265Df21227196B6f386c538b5'
+export const SWTRImplementation = '0x2f2Dd00b567b5C9d8B615fD72B68bc0Bb2030D3f'
     
